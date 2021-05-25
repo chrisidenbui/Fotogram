f248c403ac5c295ffc383832c0b9fea9b921babe
Nhan <s3687637@rmit.edu.vn>
2021-05-25 16:16:30 +0700

    Finish responsive for Homepage.html Update to test fixing imagetab_js.js

--- src/main/resources/static/JS/homepage_script.js
+++ src/main/resources/static/JS/homepage_script.js
@@ -16,15 +16,18 @@ window.addEventListener("load", () => {
 // Burger menu
 const burger = document.querySelector('.menu-button');
 const navmenu = document.querySelector('.nav-menu');
+const sidemenu = document.querySelector('.side-nav');
 let burgerclick = false;
 burger.addEventListener('click', () => {
     if (!burgerclick) {
         burger.classList.add('open');
         navmenu.classList.add('display');
+        sidemenu.classList.add('display');
         burgerclick = true;
     } else {
         burger.classList.remove('open');
         navmenu.classList.remove('display');
+        sidemenu.classList.remove('display');
         burgerclick = false;
     }
 })
--- src/main/resources/static/JS/imagetab_js.js
+++ src/main/resources/static/JS/imagetab_js.js
@@ -1,30 +1,11 @@
-var open = document.getElementById('hamburger');
-var changeIcon = true;
-document.addEventListener('DOMContentLoaded', () => {
-    // const params = (new URL(document.location)).searchParams;
-    // let image_src = document.location.search.replace(/^.*?\=/,'')
-    // let main_image = document.querySelector('.image_tab > img');
-    // main_image.src = image_src
+const heart_like = document.querySelector('.heart-icon');
+let heart_click = false;
+heart_like.addEventListener('click', () => {
+    if (!heart_click) {
+        heart_like.classList.add('open');
+        heart_click = true;
+    } else {
+        heart_like.classList.remove('open');
+        heart_click = false;
+    }
 })
-
-open.addEventListener("click", function(){
-
-    var overlay = document.querySelector('.overlay');
-    var nav = document.querySelector('nav');
-    var icon = document.querySelector('.menu-toggle i');
-
-    overlay.classList.toggle("menu-open");
-    nav.classList.toggle("menu-open");
-
-    if (changeIcon) {
-        icon.classList.remove("fa-bars");
-        icon.classList.add("fa-times");
-
-        changeIcon = false;
-    }
-    else {
-        icon.classList.remove("fa-times");
-        icon.classList.add("fa-bars");
-        changeIcon = true;
-    }
-});
