fcc0dc7858f833061b1d4c55e90f0f40b7e757b4
Nhan <s3687637@rmit.edu.vn>
2021-05-25 17:24:55 +0700

    Heart button Fix imagetab scale

--- src/main/resources/static/JS/imagetab_js.js
+++ src/main/resources/static/JS/imagetab_js.js
@@ -2,10 +2,10 @@ const heart_like = document.querySelector('.heart-icon');
 let heart_click = false;
 heart_like.addEventListener('click', () => {
     if (!heart_click) {
-        heart_like.classList.add('open');
+        heart_like.classList.add('liked');
         heart_click = true;
     } else {
-        heart_like.classList.remove('open');
+        heart_like.classList.remove('liked');
         heart_click = false;
     }
 })
